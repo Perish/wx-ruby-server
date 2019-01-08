5e81fa1ff7b3ecfffc52305b6ce861a5f112d847
Lovesx <golovesx@gmail.com>
2019-01-08 16:12:24 +0800

    index

--- app/controllers/wx_controller.rb
+++ app/controllers/wx_controller.rb
@@ -1,4 +1,7 @@
 class WxController < ApplicationController
+  def index
+    render json: {status: 200, message: "ok"}
+  end
   def recieve
     @user = User.find_by(id: params[:id])
     render text: "aaa"    
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'wx#index'
   get 'wx/:id/recieve', to: 'wx#recieve'
   post "/graphql", to: "graphql#execute"
 end

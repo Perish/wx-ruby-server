5d83b82a70eccb004c263fdfa0a2d885bfea09ca
Lovesx <golovesx@gmail.com>
2019-01-08 16:16:50 +0800

    index

--- app/controllers/wx_controller.rb
+++ app/controllers/wx_controller.rb
@@ -1,9 +1,20 @@
 class WxController < ApplicationController
+  @@token="chengzi"
+
   def index
     render json: {status: 200, message: "ok"}
   end
+
   def recieve
     @user = User.find_by(id: params[:id])
-    render text: "aaa"    
+    if check_signature?(params[:signature],params[:timestamp],params[:nonce])
+      return render text: params[:echostr]
+    end
+  end
+
+  private
+  
+  def check_signature?(signature,timestamp,nonce)
+    Digest::SHA1.hexdigest([timestamp,nonce,@@token].sort.join) == signature
   end
 end
